4fd97072b7965d6764f198deb429ac97da4f9b57
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-04-03 18:03:55 +0200

    Mediums

---
+++ Periode1/Predictif-2/src/main/java/fr/insalyon/dasi/metier/modele/Spirite.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.dasi.metier.modele;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author zakaria
+ */
+@Entity
+public class Spirite extends Medium {
+    private String support;
+
+    public Spirite(String denomination, String genre, String presentation, String support) {
+        super(denomination, genre, presentation);
+        this.support = support;
+    }
+
+    public String getSupport() {
+        return support;
+    }
+
+    public void setSupport(String support) {
+        this.support = support;
+    }
+
+    @Override
+    public String toString() {
+        return "Spirite{" + super.toString() + "support=" + support + '}';
+    }
+    
+}
