84d5feebfc8922ee64de9c7b9cbf09f4ec3024d4
Sophie Crowley <crowleso@tcd.ie>
2020-04-03 17:30:33 +0100

    Runs

--- Periode1/Predictif-1/src/main/java/fr/insalyon/dasi/metier/modele/Medium.java
+++ Periode1/Predictif-1/src/main/java/fr/insalyon/dasi/metier/modele/Medium.java
@@ -10,12 +10,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
 
 /**
  *
  * @author sophiecrowley
  */
 
+@Inheritance
 @Entity
 public abstract class Medium implements Serializable {
     @Id
