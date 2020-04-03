72254f1ff656f31b6e3ea28cf798c150fd3de737
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-04-03 18:38:41 +0200

    Brought mediums back

---
+++ Periode1/Predictif-2/src/main/java/fr/insalyon/dasi/metier/modele/Astrologue.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.dasi.metier.modele;
+
+/**
+ *
+ * @author zakaria
+ */
+public class Astrologue extends Medium {
+    private String formation;
+    private int promotion;
+
+    public Astrologue(String denomination, String genre, String presentation, 
+            String formation, int promotion) {
+        super(denomination, genre, presentation);
+        this.formation = formation;
+        this.promotion = promotion;
+    }
+    public Astrologue(){
+        
+    }
+    public String getFormation() {
+        return formation;
+    }
+
+    public void setFormation(String formation) {
+        this.formation = formation;
+    }
+
+    public int getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(int promotion) {
+        this.promotion = promotion;
+    }
+
+    @Override
+    public String toString() {
+        return "Astrologue{" + super.toString() + "formation=" + formation + ", promotion=" + promotion + '}';
+    }
+    
+}
