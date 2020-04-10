57ac35f7d8e0ed8e3190f23c734225f646b51287
Sophie Crowley <crowleso@tcd.ie>
2020-04-10 14:23:31 +0100

    Create Consultation entity

---
+++ Periode1/Predictif-1/src/main/java/fr/insalyon/dasi/metier/modele/Consultation.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.dasi.metier.modele;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author sophiecrowley
+ */
+@Entity
+public class Consultation implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;    
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    private String heureDebut;
+    private String heureFin;
+    private String commentaire;
+
+    public Long getId() {
+        return id;
+    }
+   
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getHeureDebut() {
+        return heureDebut;
+    }
+
+    public void setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+    }
+
+    public String getHeureFin() {
+        return heureFin;
+    }
+
+    public void setHeureFin(String heureFin) {
+        this.heureFin = heureFin;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    @Override
+    public String toString() {
+        return "Consultation{" + "id=" + id + ", date=" + date + ", heureDebut=" + heureDebut + ", heureFin=" + heureFin + ", commentaire=" + commentaire + '}';
+    }
+    
+    
+}
