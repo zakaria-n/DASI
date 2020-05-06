53332c64f340a873950028210dc3304afca867a3
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-05-07 01:03:52 +0200

    Frontend set-up + working login!

---
+++ Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/ActionServlet.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.predictif.ihm.web;
+
+import fr.insalyon.dasi.dao.JpaUtil;
+import fr.insalyon.predictif.ihm.web.action.Action;
+import fr.insalyon.predictif.ihm.web.action.AuthentifierClientAction;
+import fr.insalyon.predictif.ihm.web.serialisation.ProfilClientSerialisation;
+import fr.insalyon.predictif.ihm.web.serialisation.Serialisation;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author zakaria
+ */
+@WebServlet(name = "ActionServlet", urlPatterns = {"/ActionServlet"})
+public class ActionServlet extends HttpServlet {
+    
+     @Override
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.init();
+    }
+
+    @Override
+    public void destroy() {
+        JpaUtil.destroy();
+        super.destroy();
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true);
+        request.setCharacterEncoding("UTF-8");
+
+        String todo = request.getParameter("todo");
+
+        Action action = null;
+        Serialisation serialisation = null;
+
+        if (todo != null) {
+            switch (todo) {
+                case "connecter":
+                    action = new AuthentifierClientAction();
+                    serialisation = new ProfilClientSerialisation();
+                    break;
+                case "...":
+                    break;
+            }
+        }
+        
+        if (action != null) {
+            action.executer(request);
+            serialisation.serialiser(request, response);
+        }
+        else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Erreur dans les paramètres de la requête");
+        }
+
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
