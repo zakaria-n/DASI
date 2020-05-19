08d45dee7e94cc55b97048732f44f6fb5580f883
Sophie Crowley <crowleso@tcd.ie>
2020-05-19 09:17:14 +0100

    Predictions now working

--- Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/GeneratePredictions.java
+++ Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/GeneratePredictions.java
@@ -5,7 +5,13 @@
  */
 package fr.insalyon.predictif.ihm.web.action;
 
+import fr.insalyon.dasi.metier.modele.Client;
+import fr.insalyon.dasi.metier.modele.Employe;
+import fr.insalyon.dasi.metier.service.Service;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -14,6 +20,24 @@ import javax.servlet.http.HttpServletRequest;
 public class GeneratePredictions extends Action {
     @Override
     public void executer(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        List<String> predictions = null;
+        Service service = new Service();
+        int loveNum = Integer.parseInt((String) request.getParameter("loveNum"));
+        int healthNum = Integer.parseInt((String) request.getParameter("healthNum"));
+        int workNum = Integer.parseInt((String) request.getParameter("workNum"));
+        Object id = session.getAttribute("idEmploye");
+        Client c = null;
+        if(id!=null) {
+            Employe e = service.rechercherEmployeParId((long) id);
+            long clientId = service.getCurrentConsultationClient(e);
+            if(clientId!=-1) {
+                c = service.rechercherClientParId(clientId);
+                predictions = service.generatePrediction(c, loveNum, healthNum, workNum);
+            }
+        }
+        System.out.println(predictions);
+        request.setAttribute("predictions", predictions);
         
     }
 }
--- Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/serialisation/PredictionsSerialisation.java
+++ Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/serialisation/PredictionsSerialisation.java
@@ -7,9 +7,11 @@ package fr.insalyon.predictif.ihm.web.serialisation;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,9 +25,16 @@ public class PredictionsSerialisation extends Serialisation {
     
     @Override
     public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
+        List<String> predictions = (List<String>)request.getAttribute("predictions");
+        System.out.println(predictions);
         JsonObject container = new JsonObject();
-
+        if(predictions!=null) {
+            JsonObject predicts = new JsonObject();  
+            predicts.addProperty("love", predictions.get(0));
+            predicts.addProperty("health", predictions.get(1));
+            predicts.addProperty("work", predictions.get(2));
+            container.add("predictions", predicts);
+        } 
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
