120e0ba15f71c01ea3a0d85025cf5809fb889d5c
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-05-29 12:41:43 +0200

    Update CheckStartedAction.java

--- Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/CheckStartedAction.java
+++ Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/CheckStartedAction.java
@@ -5,10 +5,34 @@
  */
 package fr.insalyon.predictif.ihm.web.action;
 
+import fr.insalyon.dasi.metier.modele.Consultation;
+import fr.insalyon.dasi.metier.modele.Employe;
+import fr.insalyon.dasi.metier.service.Service;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author zakaria
  */
-public class CheckStartedAction {
-    
+public class CheckStartedAction extends Action {
+    @Override
+    public void executer(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        boolean success = false;
+        Service service = new Service();
+        Long id = (Long) session.getAttribute("idEmploye");
+        System.out.println("Employe "+ id);
+        if(id!=null) {
+            Employe e = service.rechercherEmployeParId(id);
+            Consultation consul = service.getCurrentConsultation(e);//e.getConsultations().get(0);
+            if (consul != null)
+            {
+                success = service.confirmConsultation(consul);
+            }
+        }else{
+            request.setAttribute("notLoggedIn", true);
+        }
+        request.setAttribute("success", success); 
+    }
 }
