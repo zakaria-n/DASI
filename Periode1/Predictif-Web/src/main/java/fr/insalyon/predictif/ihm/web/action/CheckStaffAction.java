80fa99bd83433ba4d56210b52110c5f7d67e682d
Sophie Crowley <crowleso@tcd.ie>
2020-05-29 14:32:49 +0100

    Revert "Merge branch 'master' of https://github.com/zakaria-n/DASI"
    This reverts commit 89d43f6cc9aa74945f863b97faeb4573276887f4, reversing
    changes made to a4d296073edabb27aa3834d1723260f2c0e5b703.

--- Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/CheckStaffAction.java
+++ Periode1/Predictif-Web/src/main/java/fr/insalyon/predictif/ihm/web/action/CheckStaffAction.java
@@ -5,9 +5,6 @@
  */
 package fr.insalyon.predictif.ihm.web.action;
 
-import fr.insalyon.dasi.metier.modele.Consultation;
-import fr.insalyon.dasi.metier.modele.Employe;
-import fr.insalyon.dasi.metier.service.Service;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -18,17 +15,10 @@ import javax.servlet.http.HttpSession;
 public class CheckStaffAction extends Action {
     @Override
     public void executer(HttpServletRequest request) {
-        Service service = new Service();
         HttpSession session = request.getSession(); 
         Long id = (Long) session.getAttribute("idEmploye");
         if(id==null) {
             request.setAttribute("notLoggedIn", true);
-        } else {
-            Employe e = service.rechercherEmployeParId(id); 
-            Consultation c = service.getCurrentConsultation(e);
-            if(c!=null){
-                request.setAttribute("inConsultation", true);
-            }
         }
     }
 }
